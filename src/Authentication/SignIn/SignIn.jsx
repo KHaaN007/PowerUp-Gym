a1071058767d3ef301cd412462a367b023231a19
KHaaN007 <khaanmr007@gmail.com>
2023-11-24 17:10:16 +0600

    Firebase Auth Added

--- src/Authentication/SignIn/SignIn.jsx
+++ src/Authentication/SignIn/SignIn.jsx
@@ -1,8 +1,61 @@
+import { Link } from "react-router-dom";
+
 const SignIn = () => {
     return (
         <div>
-            <h2>This Is Sign In Page For Login User</h2>
             
+            {/* <!-- component --> */}
+            {/* <!-- page --> */}
+            <div
+                className="mx-auto flex min-h-screen w-full items-center justify-center bg-gray-900 text-white"
+            >
+                {/* <!-- component --> */}
+                <from className="flex w-[30rem] flex-col space-y-10">
+                    <div className="text-center text-4xl font-medium">Log In</div>
+
+                    <div
+                        className="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500"
+                    >
+                        <input
+                            type="text"
+                            placeholder="Email or Username"
+                            className="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"
+                        />
+                    </div>
+
+                    <div
+                        className="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500"
+                    >
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            className="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"
+                        />
+                    </div>
+
+                    <button
+                        className="transform rounded-sm bg-indigo-600 py-2 font-bold duration-300 hover:bg-indigo-400"
+                    >
+                        LOG IN
+                    </button>
+
+                    <a
+                        href="#"
+                        className="transform text-center font-semibold text-gray-500 duration-300 hover:text-gray-300"         >FORGOT PASSWORD?
+                    </a>
+
+
+                    <p className="text-center text-lg">
+                        No account?
+                        <Link
+                        to='/signUp'
+                            href="#"
+                            className="font-medium text-indigo-500 underline-offset-4 hover:underline"
+                        >Create One
+                        </Link>
+                    </p>
+                </from>
+            </div>
         </div>
     );
 };
--- src/Authentication/SignUp/SignUp.jsx
+++ src/Authentication/SignUp/SignUp.jsx
@@ -1,20 +1,33 @@
+import { Link } from "react-router-dom";
+
 const SignUp = () => {
+
+
+
+
+
+
     return (
         <div>
+
             {/* <!-- component --> */}
             {/* <!-- page --> */}
             <div
-                className="mx-auto flex min-h-screen w-full items-center justify-center text-white"
-                style={{
-                    backgroundImage: "url('https://ibb.co/5jNTgRZ')",
-                    backgroundSize: "cover",
-                    backgroundPosition: "center",
-                }}
+                className="mx-auto flex min-h-screen w-full items-center justify-center bg-gray-900 text-white"
             >
                 {/* <!-- component --> */}
-                <section className="flex w-[30rem] flex-col space-y-10">
-                    <div className="text-center text-4xl font-medium">Log In</div>
+                <form className="flex w-[30rem] flex-col space-y-10">
+                    <div className="text-center text-4xl font-medium">SignUp</div>
 
+                    <div
+                        className="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500"
+                    >
+                        <input
+                            type="text"
+                            placeholder="Username"
+                            className="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"
+                        />
+                    </div>
                     <div
                         className="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500"
                     >
@@ -41,21 +54,19 @@ const SignUp = () => {
                         LOG IN
                     </button>
 
-                    <a
-                        href="#"
-                        className="transform text-center font-semibold text-gray-500 duration-300 hover:text-gray-300"         >FORGOT PASSWORD?
-                    </a>
 
 
                     <p className="text-center text-lg">
-                        No account?
-                        <a
+                        Have account ?
+
+                        <Link
+                            to='/signIn'
                             href="#"
                             className="font-medium text-indigo-500 underline-offset-4 hover:underline"
-                        >Create One
-                        </a>
+                        > Login
+                        </Link>
                     </p>
-                </section>
+                </form>
             </div>
         </div>
     );
